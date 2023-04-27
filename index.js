26260b24d47441026783e3b77bdc5e5f4c81438c
okiki <companychris00@gmail.com>
2023-04-27 04:48:11 +0100

    correction

--- index.js
+++ index.js
@@ -5,45 +5,6 @@ if(process.env.NODE_ENV !== 'production'){
 const express =  require('express');
 const app = express();
 app.use(express.urlencoded({extended: true}))
-// const transporter = nodemailer.createTestAccount({
-//     service: 'gmail', 
-//     auth: {
-//         user: 'companychris00@gmail.com',
-//         pass: 'wdcdawjhdxbxfsea'
-//     }
-// })
-// app.post('/submit', async (req, res) => {
-//     const {name, userEmail, subject, message } = req.body
-  
-//     try {
-       
-
-//         const mailOptions = {
-//             from: 'companychris00@gmail.com',
-//             to: 'companychris00@gmail.com',
-//             subject: 'new registration',
-//             html: `<p>name: ${name}</p> \n <p>user-email: ${userEmail}</p> \n
-//             <p>subject: ${subject}</p> \n <p>message: ${message}</p>
-//             `
-//         }
-
-//        await transporter.sendMail(mailOptions, (err) => {
-//             if(err) {
-//                 console.log('this is the eroor', err)
-//             } else {
-//                 console.log('email sent successfully');
-//                 res.json('email sent succesfully')
-//             };
-//         });
-                    
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json('error from the backend')
-//     }
-   
-
-//     console.log(req.body)
-// })
 
 const nodemailer = require('nodemailer');
 
@@ -52,8 +13,6 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS
-    // user: 'imperialtotalsolution@gmail.com',
-    // pass: 'smzbqszggkvtuhgd'
   }
 });
 
